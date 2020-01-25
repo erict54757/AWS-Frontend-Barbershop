aeab9b85275c36d442b1b99ac30f12015dde3c29
eric taft <erict54757@gmail.com>
2020-01-25 09:57:17 -0500

    uploaded env

--- src/components/GoogleMapComponent/MapWithADirectionsRenderer.js
+++ src/components/GoogleMapComponent/MapWithADirectionsRenderer.js
@@ -13,7 +13,7 @@ const {
 const MapWithADirectionsRenderer = compose(
   withProps({
     
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBmyFxAnT58RzMepjHQIw6UPS8ToDQGspw&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key="+process.env.REACT_APP_GOOGLE_API_KEY+"&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `340px` }} />,
     mapElement: <div style={{ height: `90%`,marginTop:"25px",borderRadius:"5px" }} />,
